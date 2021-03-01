426bd98f6bf2d9868e8a31d8f1b9fcf41368f07c
sergio núñez meneses <63654322+sergio-nunez-meneses@users.noreply.github.com>
2021-03-01 22:54:56 +0100

    added a for loop to create user and structure data fixtures

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -18,6 +18,23 @@ class UserFixtures extends Fixture
         $this->passwordEncoder = $passwordEncoder;
     }
 
+    public function int_to_ordinal($i)
+    {
+      $j = $i % 10;
+      $k = $i % 100;
+
+      if ($j === 1 && $k !== 11) {
+        return $i . 'st';
+      }
+      if ($j === 2 && $k !== 12) {
+        return $i . 'nd';
+      }
+      if ($j === 3 && $k !== 13) {
+        return $i . 'rd';
+      }
+      return $i . 'th';
+    }
+
     public function load(ObjectManager $manager)
     {
         $usernames = [
@@ -33,13 +50,6 @@ class UserFixtures extends Fixture
 
         $password = 'Dø€På∫∫!';
 
-        $structures = [
-          'My First Structure',
-          'My Second Structure',
-          'My Third Structure',
-          'My Fourth Structure'
-        ];
-
         $roles = [
           'ROLE_SUPERADMIN',
           'ROLE_ADMIN',
@@ -47,24 +57,34 @@ class UserFixtures extends Fixture
           'ROLE_USER'
         ];
 
-        // set data
-        $structure = new Structure();
-        $structure->setName($structures[0]);
+        for ($i=0; $i < count($usernames); $i++)
+        {
+          $random_percentage = mt_rand(0, 100);
+          $random_role = mt_rand(0, (count($roles) - 1));
 
-        $user = new User();
-        $user->setUsername($usernames[0]);
-        $user->setPassword($this->passwordEncoder->encodePassword(
-            $user,
-            $password . '1'
-        ));
-        $user->setRoles(array($roles[1]));
+          // set data
+          $structure = new Structure();
+          $structure->setName('My ' . $this->int_to_ordinal($i + 1) . ' structure');
 
-        // relate this user to the structure
-        $user->setStructure($structure);
+          $user = new User();
+          $user->setUsername($usernames[$i]);
+          $user->setPassword($this->passwordEncoder->encodePassword(
+              $user,
+              $password . $i
+          ));
+          $user->setRoles(array(
+            ($random_percentage > 60) ? $roles[$random_role] : $roles[3]
+          ));
 
-        $manager->persist($structure);
-        $manager->persist($user);
+          // relate this user to the structure
+          $user->setStructure($structure);
 
+          // manage data
+          $manager->persist($structure);
+          $manager->persist($user);
+        }
+
+        // save data
         $manager->flush();
     }
 }
