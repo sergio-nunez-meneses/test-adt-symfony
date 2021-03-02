e77dec657a9f03f389653c48c84aa51d18c3e32a
sergio núñez meneses <63654322+sergio-nunez-meneses@users.noreply.github.com>
2021-03-02 12:27:47 +0100

    added user controller, show method and show template

---
+++ src/Controller/UserController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+use App\Entity\User;
+
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/user", name="user")
+     */
+    public function index(): Response
+    {
+        return $this->render('user/index.html.twig', [
+            'controller_name' => 'UserController',
+        ]);
+    }
+
+    /**
+     * @Route("/user/{id}", name="user_show")
+     */
+    public function show(int $id): Response
+    {
+        $user = $this->getDoctrine()
+            ->getRepository(User::class)
+            ->find($id);
+
+        if (!$user) {
+            throw $this->createNotFoundException(
+                "No user found for id $id"
+            );
+        }
+
+        return $this->render('user/show.html.twig', ['user' => $user]);
+    }
+}
